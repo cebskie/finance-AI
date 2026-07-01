3641ee5c62d8d7a3f4a03afed946fa76ae330792
cebskie <ceiba.abdillah@gmail.com>
2026-07-02 00:20:53 +0700

    chore: connect FE to BE

--- backend/app/main.py
+++ backend/app/main.py
@@ -28,8 +28,9 @@ from fastapi.middleware.cors import CORSMiddleware
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
+    origins = [
+        "http://localhost:3000",                    # Local development
+        "https://finance-ai-gamma-two.vercel.app",  # Production frontend
     ],
     allow_credentials=True,
     allow_methods=["*"],
